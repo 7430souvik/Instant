5ba50e39e5933eb9317acfe56ca2db33e96d9117
Souvik Chatterjee <souvikchatterjee080@gmail.com>
2026-09-21 19:52:56 +0530

    changed home image address

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -65,7 +65,7 @@ function HomeComponent() {
 
             </div>
             <div className='rightPanel'>
-                <img srcSet='public/home.png' alt=''/>
+                <img srcSet='/home.png' alt=''/>
 
             </div>
 
